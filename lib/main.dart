52143b3c39c2d56ea15bf712db32b80bfe4d61d8
agrahafiz13 <agrahafiz2@gmail.com>
2026-02-15 16:51:45 +0700

    update main pada method build

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flame/game.dart';
-import 'game/fruit_catcher_game.dart';
 
-void main() {
+import 'game/fruit_catcher_game.dart';
+import 'game/managers/audio_manager.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await AudioManager().initialize();
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: GameScreen(),
-    );
-  }
+  State<MyApp> createState() => _MyAppState();
 }
 
-class GameScreen extends StatefulWidget {
-  const GameScreen({super.key});
-
-  @override
-  State<GameScreen> createState() => _GameScreenState();
-}
-
-class _GameScreenState extends State<GameScreen> {
+class _MyAppState extends State<MyApp> {
   late FruitCatcherGame game;
 
   @override
@@ -35,52 +27,57 @@ class _GameScreenState extends State<GameScreen> {
   }
 
   @override
+  void dispose() {
+    game.onRemove();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          GameWidget(game: game),
-          Positioned(
-            top: 50,
-            left: 20,
-            child: Container(
-              padding: const EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                color: Colors.black54,
-                borderRadius: BorderRadius.circular(10),
-              ),
+    return MaterialApp(
+      home: Scaffold(
+        body: Stack(
+          children: [
+            GameWidget(game: game),
+            Positioned(
+              top: 50,
+              left: 20,
               child: ValueListenableBuilder<int>(
                 valueListenable: game.scoreNotifier,
-                builder: (context, score, child) {
+                builder: (context, score, _) {
                   return Text(
                     'Score: $score',
                     style: const TextStyle(
-                      color: Colors.white,
                       fontSize: 24,
                       fontWeight: FontWeight.bold,
+                      color: Colors.black,
                     ),
                   );
                 },
               ),
             ),
-          ),
-          Positioned(
-            top: 50,
-            right: 20,
-            child: Row(
-              children: [
-                IconButton(
-                  icon: const Icon(Icons.music_note, color: Colors.black),
-                  onPressed: () {},
-                ),
-                IconButton(
-                  icon: const Icon(Icons.volume_up, color: Colors.black),
-                  onPressed: () {},
-                ),
-              ],
+            Positioned(
+              top: 50,
+              right: 20,
+              child: Row(
+                children: [
+                  IconButton(
+                    icon: const Icon(Icons.music_note, color: Colors.black),
+                    onPressed: () {
+                      AudioManager().toggleMusic();
+                    },
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.volume_up, color: Colors.black),
+                    onPressed: () {
+                      AudioManager().toggleSfx();
+                    },
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
